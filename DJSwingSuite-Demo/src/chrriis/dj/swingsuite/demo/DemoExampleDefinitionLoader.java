fc764726d467b17fd49c35877c26d1533704b1e4
Chrriis <chrriis@gmail.com>
2009-04-29 20:49:09 +0000

    Re-ordered menu items.

--- DJSwingSuite-Demo/src/chrriis/dj/swingsuite/demo/DemoExampleDefinitionLoader.java
+++ DJSwingSuite-Demo/src/chrriis/dj/swingsuite/demo/DemoExampleDefinitionLoader.java
@@ -30,10 +30,6 @@ public class DemoExampleDefinitionLoader {
     exampleGroupList.add(new ExampleGroup("Introduction", new Example[] {
         new Example("Swing Suite", SwingSuite.class, "The motivations behind this project.", false),
     }));
-    exampleGroupList.add(new ExampleGroup("Entry Fields", new Example[] {
-        new Example("Text and Numbers", TextAndNumberFieldsExample.class, "This is a simple example that shows text and number fields.", true),
-        new Example("Validators, Formatters, Masks", ValidatorsFormattersAndMasksExample.class, "How to use validators, formatters and masks to control inputs and improve usability.", true),
-    }));
     exampleGroupList.add(new ExampleGroup("Toolbox", new Example[] {
         new Example("Combo Button", ComboButtonExample.class, "Combo buttons are generally found in tool bars, often to offer a list of choices and optionally a default action.", true),
         new Example("Tri-state check box", TriStateCheckBoxExample.class, "Tri-state check boxes, with advanced capabilities like rolling cycle definition.", true),
@@ -41,6 +37,10 @@ public class DemoExampleDefinitionLoader {
         new Example("Wide popup combo box", WidePopupComboBoxExample.class, "Wide popup combo boxes are a solution to the problem of having content that is too long. Such content generally messes up the user interface layout.", true),
         new Example("Selectable Tree", SelectableTreeExample.class, "A tree where the user can click in the empty area, and drag the mouse over some nodes to select them. Modifiers (control and shift) can add or exclude to the current selection.", true),
         new Example("Titled Separator", TitledSeparatorExample.class, "Titled separators are generally more elegant than titled borders and do not require nesting panels.", true),
+    }));
+    exampleGroupList.add(new ExampleGroup("Entry Fields", new Example[] {
+        new Example("Text and Numbers", TextAndNumberFieldsExample.class, "This is a simple example that shows text and number fields.", true),
+        new Example("Validators, Formatters, Masks", ValidatorsFormattersAndMasksExample.class, "How to use validators, formatters and masks to control inputs and improve usability.", true),
     }));
     return exampleGroupList;
   }
